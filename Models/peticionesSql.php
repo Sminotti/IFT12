5d3086578de28b2a66b1c1b2987fe7d53877d911
Sminotti <sebastianminotti@gmail.com>
2024-07-30 19:57:26 -0300

    solucionado vista del JSON en Back-End

--- Models/peticionesSql.php
+++ Models/peticionesSql.php
@@ -9,13 +9,13 @@
     //$password = password_hash($_POST['password'], PASSWORD_BCRYPT,['cost' => 10])?? null;// buscar otra forma no deprecated...
     $password= $_POST['password'] ?? null;
     // LOGIN //
-    $login = "SELECT * FROM usuario WHERE usuario=$usuario AND password=$password AND habilitado=1 AND eliminado = 0";
+    $login = "SELECT usuario,password FROM usuario WHERE usuario=$usuario AND password=$password AND habilitado=1 AND eliminado = 0";
     // REGISTRO DE USUARIO //
     $registroUsuario = "INSERT INTO usuario,persona (usuario,password,nombre,apellido,edad,dni) VALUES ('$usuario','$password','$nombre','$apellido','$edad','$dni') ";
     // LISTAR REGISTROS PERSONAS//
-    $listar = "SELECT * FROM persona WHERE habilitado=1 AND eliminado = 0 ORDER BY idPersona DESC ";
+    $listar = "SELECT idPersona,apellido,nombre,edad,dni FROM persona WHERE habilitado=1 AND eliminado = 0 ORDER BY idPersona DESC ";
     // EDITAR REGISTRO PERSONAS//
-    $listarRegistro = "SELECT * FROM persona WHERE idPersona='$idPersona'AND habilitado=1 AND eliminado = 0";
+    $listarRegistro = "SELECT idPersona,apellido,nombre,edad,dni FROM persona WHERE idPersona='$idPersona'AND habilitado=1 AND eliminado = 0";
     $editar = "UPDATE persona SET nombre='$nombre',apellido='$apellido',edad='$edad',dni='$dni' WHERE idPersona='$idPersona' ";
     //ELIMINAR REGISTRO PERSONAS//
     $eliminar = "UPDATE persona SET habilitado=0 ,eliminado=1 WHERE idPersona='$idPersona'";
--- Views/listado-Back-End.php
+++ Views/listado-Back-End.php
@@ -10,8 +10,9 @@ $idPersona = $_GET['idPersona'] ?? null;
 
   // Listar personas
   $listarRegistros = mysqli_query($conectarDB, $listar);
-  $registros =mysqli_fetch_array($listarRegistros);
-  $mostrarRegistros = json_encode($registros);
-
-  echo json_encode ($mostrarRegistros);
+  //$registros =mysqli_fetch_array($listarRegistros);
+ // $mostrarRegistros = json_encode($registros);
+ $registros =mysqli_fetch_all($listarRegistros,MYSQLI_ASSOC);
+ //$registros =mysqli_fetch_assoc($listarRegistros);
+  echo json_encode ($registros);
 ?>  
